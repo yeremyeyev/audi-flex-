8049d560056489397a40675371c0bb890b55ab8b
Eremeev Andrey <a.yeremyeyev@gmail.com>
2019-10-07 15:13:38 +0300

    Загружаю проект

---
+++ js/js.js
@@ -0,0 +1,11 @@
+$('.slider-1').slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    dots: true,
+    infinite: true,
+    // adaptiveHeight: true,
+    // variableWidth: true,
+    // dotsClass: ''
+    // appendDots:'dots-container'
+     
+});
